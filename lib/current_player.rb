9e714a544b56ef2d4db9c41f59ac6b5d3c483ec2
Mark Noonan <markthomasnoonan@gmail.com>
2018-06-13 17:54:59 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,6 +1,8 @@
 def turn_count(board)
   turns_played = 0
-  board.each do |space|
+  board.each do |space_content|
+    if space_content == "X" || space_content == "O"
+      turns_played += 1
   end
 end
 
